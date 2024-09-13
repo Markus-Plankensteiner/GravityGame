fb3e586cd3e98faa0849b518f955f58cde5ca807
Markus-Plankensteiner <markusplankensteiner97@gmail.com>
2024-09-13 08:32:07 +0200

    Initial commit
    Has a bunch of stuff already in, but now I'd rather not want to lose it ;)

---
+++ Source/GravityGame/Private/GravityGameFunctionLibrary.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GravityGame/Public/GravityGameFunctionLibrary.h"
+
+void UGravityGameFunctionLibrary::RerunConstructionScripts(AActor* Target)
+{
+	if (Target) {
+		Target->RerunConstructionScripts();
+	}
+}
---
+++ Source/GravityGame/Public/GravityGameFunctionLibrary.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GravityGameFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAVITYGAME_API UGravityGameFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void RerunConstructionScripts(AActor* Target);
+};
